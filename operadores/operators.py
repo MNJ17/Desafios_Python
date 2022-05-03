ebbb24661322ca2fa202f1d18a31ac2e1a1b9c3a
Monaliza <nascimentodejesusmonaliza522@gmail.com>
2022-05-03 00:36:07 -0300

    Utilizando operadores matemáticos em python

---
+++ operadores/operators.py
@@ -0,0 +1,40 @@
+def add(first_number, second_number):
+
+  return first_number + second_number
+  pass
+  #add (5, 3)
+
+def sub(first_number, second_number):
+
+  return first_number - second_number
+  pass
+  #add (6, 4)
+
+def mult(first_number, second_number):
+
+  return first_number * second_number
+  pass
+  #add (4, 3)
+
+def div(first_number, second_number):
+
+  return first_number / second_number
+  pass
+  #add (2, 4)
+
+def expo(first_number, second_number):
+
+  return first_number ** second_number
+  pass
+  #add (4, 3)
+
+def remai(first_number, second_number):
+
+  return first_number % second_number
+  pass
+
+def quoti_remai(first_number, second_number):
+
+  return first_number // second_number, first_number % second_number
+  pass
+
