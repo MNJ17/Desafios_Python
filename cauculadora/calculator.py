3000199d3227f7c7411a7c7ecd097542570b9be2
Monaliza <nascimentodejesusmonaliza522@gmail.com>
2022-05-03 10:00:53 -0300

    Utilizando o operador de adição e o input do usuário para criar uma calculadora

---
+++ cauculadora/calculator.py
@@ -0,0 +1,6 @@
+def calculator():
+  # TODO: Retorne o resultado da adição dos números informados pelo usuário
+  n1 = input ("digite o primeiro número: ")
+  n2 = input ("digite o segundo número: ")
+  return n1 + n2
+  pass
