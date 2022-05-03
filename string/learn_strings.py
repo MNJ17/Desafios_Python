d6b18852ac18229e8606c90c0ddb1ad8a06ed203
Monaliza <nascimentodejesusmonaliza522@gmail.com>
2022-05-03 09:52:39 -0300

    Utilizando métodos em strings e aprender a pesquisar métodos em Python

---
+++ string/learn_strings.py
@@ -0,0 +1,25 @@
+
+def has_letter(letter, input_word):
+  # TODO: Retorne True ou False para a existência da letra (letter) no texto informado (string)
+  return (letter in input_word)
+  pass
+
+def uper_case(input_word):
+
+  return (input_word.upper())
+  pass
+
+def lower_case(input_word):
+
+  return (input_word.lower())
+  pass
+
+def capitalize(input_word):
+
+  return (input_word.title())
+  pass
+
+def reverse(input_word):
+
+  return (input_word [::-1])
+  pass
